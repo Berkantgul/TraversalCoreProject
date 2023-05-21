4eaaa38013dd43f10dc46b2fb03b252b97983a7b
Berkant Gul <berkantgul25@gmail.com>
2023-05-21 14:55:13 +0300

    Add project files.

---
+++ TraversalCoreProject.Entity/Concrete/SecondAbout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.Entity.Concrete
+{
+	public class SecondAbout
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string SecondaTitle { get; set; }
+		public string Description { get; set; }
+		public string Image { get; set; }
+	}
+}
