68edbad2b4f519edd3384bccade6ef270d64f5e4
Berkant Gul <berkantgul25@gmail.com>
2023-05-22 11:14:25 +0300

    Defining the Context class

---
+++ TraversalCoreProject.DataAccess/Concrete/Context.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraversalCoreProject.Entity.Concrete;
+
+namespace TraversalCoreProject.DataAccess.Concrete
+{
+	public class Context : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=LAPTOP-UROTH5AA;initial catalog=EasyCashDb;integrated Security=true");
+		}
+
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Destination> Destinations { get; set; }
+		public DbSet<FirstAbout> FirstAbouts { get; set; }
+		public DbSet<FirstFeature> FirstFeatures { get; set; }
+		public DbSet<Guide> Guides { get; set; }
+		public DbSet<Newsletter> Newsletters { get; set; }
+		public DbSet<SecondAbout> SecondAbouts { get; set; }
+		public DbSet<SecondFeature> SecondFeatures { get; set; }
+		public DbSet<SubAbout> SubAbouts { get; set; }
+		public DbSet<Testimaniol> Testimaniols { get; set; }
+	}
+}
