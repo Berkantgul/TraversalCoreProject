79c88826289c4a409b64630a228094250e32fb48
Berkant Gul <berkantgul25@gmail.com>
2023-06-01 13:01:58 +0300

    Banner area in Default/Index moved to ViewComponent

---
+++ TraversalCoreProject.Presentation/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Presentation.ViewComponents.Default
+{
+    public class _SliderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
